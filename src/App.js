db02a9491a061493401de09e51f09d37dea99735
Jinsu Park <bo314@naver.com>
2020-12-03 10:35:59 +0900

    Update: event schema

--- src/App.js
+++ src/App.js
@@ -5,8 +5,9 @@ import React from 'react'
 import {Container, Row, Col, Button, Form, Image} from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.css';
 import axios from "axios";
-
+import websocketHandler from './websocketHandler';
 const HOST = "go.oss.jinsu.me"
+// const HOST = "localhost"
 const PORT = 1323
 
 class App extends React.Component {
@@ -28,7 +29,6 @@ class App extends React.Component {
         ...state, freelancers:  results.data.sort((f)=>f['ID'])
       }})
     })()
-    
 
     if (window["WebSocket"]) {
       let conn = new WebSocket(`ws://${HOST}:${PORT}/ws`);
@@ -39,18 +39,7 @@ class App extends React.Component {
       };
       
       conn.onmessage = function (evt) {
-        let freelancer = JSON.parse(evt.data)
-        // console.log(evt)
-        let freelancers = self.state['freelancers']
-        let index = freelancers.findIndex((f)=>f['ID']==freelancer['ID'])
-        if(index == -1){
-          freelancers.push(freelancer)
-        } else{
-          freelancers[index] = freelancer
-        }
-        self.setState(state=>{return {
-          ...state, freelancers:  freelancers.sort((f)=>f['ID'])}
-        })
+        websocketHandler.messageHandler(self, evt);
       };
   } else {
       var item = document.createElement("div");
---
+++ src/websocketHandler.js
@@ -0,0 +1,28 @@
+function messageHandler(self, event) {
+    let message = JSON.parse(event.data)
+    // event.data에는 서버에서 전송한 JSON string이 들어있다.
+    // 이는 Type: string, Data: JSON object or array이다.
+    // Type에 따른 핸들러를 실행.
+    if(message.Type == "freelancer_state_report"){
+        onWorkerStateReportHandler(self, message)
+    }
+}
+
+function onWorkerStateReportHandler(self, message){
+    let freelancer = message.Data
+    console.log(freelancer)
+    let freelancers = self.state['freelancers']
+    let index = freelancers.findIndex((f)=>f['ID']==freelancer['ID'])
+    if(index == -1){
+        freelancers.push(freelancer)
+    } else{
+        freelancers[index] = freelancer
+    }
+    self.setState(state=>{return {
+        ...state, freelancers:  freelancers.sort((f)=>f['ID'])}
+    })
+}
+
+export default {
+    'messageHandler': messageHandler
+}
